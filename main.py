74191b057fd54f49404b544a6866dbad452181d6
Zakhar <38567289+maZe0z@users.noreply.github.com>
2022-08-22 15:02:57 +0300

    Better profile handling

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import io
 import sys
 import logging
 from os import *
+from json import load, dump
 from PyQt6 import QtWidgets
 from PyQt6.QtCore import Qt
 from ui import Ui_MainWindow
@@ -19,25 +21,29 @@ class MainWindow(QtWidgets.QMainWindow):
     def init_UI(self):
         logging.basicConfig(level=logging.INFO)
         self.setFixedSize(self.size())
-        self.ui.lineEdit.setText("Default")
         self.ui.label.setStyleSheet("color: red")
         self.ui.pushButton.setIcon(QIcon(r"img\folder.png"))
         self.ui.searchButton.clicked.connect(self.run_clicker)
         self.ui.pushButton.clicked.connect(self.get_chrome_user_data)
-        self.ui.lineEdit.editingFinished.connect(self.set_chrome_profile)
         self.chrome_user_data = self.check_default_path()
         self.chrome_profile = "Default"
         self.setWindowTitle("NearCrowd Review Clicker")
         self.setWindowIcon(QIcon(r"img\nearcrowd.jpg"))
+        self.profiles = self.load_profiles()
+        self.ui.comboBox.currentIndexChanged.connect(self.set_chrome_profile)
 
     def set_chrome_profile(self):
-        self.chrome_profile = self.ui.lineEdit.text()
-        print(self.chrome_profile)
+        for key, value in self.profiles.items():
+            if value == self.ui.comboBox.currentText():
+                self.chrome_profile = key
+                print(key)
+                break
 
     def run_clicker(self):
         if self.check_empty_fields() and self.check_chrome_profile(
-            self.ui.lineEdit.text()
+            self.ui.comboBox.currentText()
         ):
+            print(self.chrome_profile)
             clicker = ReviewClicker(self.chrome_user_data, self.chrome_profile)
             clicker.load_page()
             clicker.find_review()
@@ -57,22 +63,52 @@ class MainWindow(QtWidgets.QMainWindow):
             return self.get_chrome_user_data()
 
     def check_chrome_profile(self, profile_name):
-        path = self.chrome_user_data
-
-        for f in scandir(path=path):
-            print(f"{f.name} - {profile_name}")
-            if f.is_dir() and f.name == profile_name:
+        for i in self.profiles:
+            print(i)
+            if self.profiles[i] == profile_name:
                 return True
-
-        self.show_incorrect_profile_dialog()
         return False
 
     def check_empty_fields(self):
-        if self.ui.lineEdit.text() == "" or self.chrome_user_data == "":
+        if self.chrome_user_data == "":
             self.show_empty_fields_dialog()
             return False
         else:
             return True
+
+    def get_profile_name_from_file(self, file_path):
+        with io.open(file_path, "r", encoding="utf-8") as f:
+            data = load(f)
+
+        return data["profile"]["name"]
+
+    def load_profiles(self):
+        path = self.chrome_user_data
+        real_profile_name = ""
+
+        profiles = dict()
+
+        for f in scandir(path=path):
+            if f.name.__contains__("Profile") or f.name.__contains__("Default"):
+
+                if f.name == "Guest Profile" or f.name == "System Profile":
+                    continue
+
+                initial_profile_name = f.name
+                chrome_profile_name_path = rf"{path}\{initial_profile_name}\Preferences"
+
+                real_profile_name = self.get_profile_name_from_file(
+                    chrome_profile_name_path
+                )
+
+                # print(f"\n{initial_profile_name} :\n {real_profile_name}\n")
+
+                # print(chrome_profile_name_path)
+
+                profiles[f.name] = real_profile_name
+                self.ui.comboBox.addItem(real_profile_name)
+
+        return profiles
 
     def show_empty_fields_dialog(self):
         msgBox = QMessageBox()
